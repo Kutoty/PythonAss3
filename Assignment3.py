11a60d598c1d4dd31416ab4057b6ea5c9f5fc6c5
FRANCIS KISIO <40833770+Kutoty@users.noreply.github.com>
2021-04-06 21:05:45 +0300

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,16 +1,15 @@
-#Enter Full Names
 print("Enter First and Last Name: ")
 fname = input("fname: ") 
 lname = input("lname: ")
 fullnames = (fname + " " + lname)
 print(fullnames)
-#Enter phone, email
+
 print("Enter Customer's Phone Number: ")
 phone = input("phone: ")
+
 print("Enter Customer's email address: ")
 email = input("email: ")
 
-#price of a used car
 price = 10000
 has_good_credit = True;
 
